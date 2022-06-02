c15d80b44e0d2e4f39b3a10c8b2734814b0dd3c2
Oleg Morev <job.morev@gmail.com>
2022-06-02 15:35:19 +0400

    update

--- bird.js
+++ bird.js
@@ -1,39 +1,55 @@
-function birdsCreate(numberBirds) {
+const BIRD_YELL = ['Hello!','Shalom!','Aloha!','Bom dia!','Salute!','Guten Tag!','Hola!','Bonjour!','Zdravo!','Terve!'];
 
-    const BIRD_YELL = ['Hello!','Shalom!','Aloha!','Bom dia!','Salute!','Guten Tag!','Hola!','Bonjour!','Zdravo!','Terve!'];
+const scene = document.querySelector('.scene');
 
-    const scene = document.querySelector('.scene');
+//create bird class
+class Bird {
     
-    //create class bird 
-    class Bird {
-    
-        constructor(option) {
-    
-            this.coordinateY = option.coordinateY;
-            this.coordinateX = option.coordinateX;
-            this.color = option.color;
-            this.yell = option.yell;  
-        }
+    constructor(option) {
+
+        this.coordinateY = option.coordinateY;
+        this.coordinateX = option.coordinateX;
+        this.color = option.color;
+        this.yell = option.yell;  
     }
+}
+
+//random number generation for color/yell
+function generationNumber(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+//create bird object
+function createBird() {
+
+    let bird = new Bird({
     
+        coordinateY: Math.round((Math.random() * 100)),
+        coordinateX: Math.round((Math.random() * 100)),
+        color: generationNumber(1,10),
+        yell: BIRD_YELL[generationNumber(0,9)]
+    });
+
+    scene.insertAdjacentHTML('beforeEnd', 
+    `<figure class="bird bird--color-` + bird.color + `" style="top: ` + bird.coordinateY + `%; left: ` + bird.coordinateX + `%;">
+        <figure class="bird__body"></figure>
+        <figure class="bird__head">
+            <figure class="eye eye--left"></figure>
+            <figure class="eye eye--right"></figure>
+        </figure>
+        <figure class="bird__wing bird__wing--left"></figure>
+        <figure class="bird__wing bird__wing--right"></figure>
+        <figure class="bird__yell">` + bird.yell + `</figure>
+    </figure>`); 
+}
+
+//bird generation
+function getBirds(numberBirds) {
+
     for (let i = 0; i < numberBirds; i++) { 
     
-        let bird = new Bird({
-    
-            coordinateY: Math.round((Math.random() * 100)),
-            coordinateX: Math.round((Math.random() * 100)),
-            color: numberGeneration(1,10),
-            yell: BIRD_YELL[numberGeneration(0,9)]
-    
-        });
-    
-        scene.insertAdjacentHTML('beforeEnd', '<figure class="bird bird--color-' + bird.color + '" style="top: '+ bird.coordinateY + '%; left: ' + bird.coordinateX + '%;"><figure class="bird__body"></figure><figure class="bird__head"><figure class="eye eye--left"></figure><figure class="eye eye--right"></figure></figure><figure class="bird__wing bird__wing--left"></figure><figure class="bird__wing bird__wing--right"></figure><figure class="bird__yell">' + bird.yell + '</figure></figure>'); 
-    }
-    
-    //random number generation for color/yell
-    function numberGeneration(min, max) {
-        return Math.round(Math.random() * (max - min)) + min;
-    }
+        createBird();
+    };
     
     //bird animation
     const birds = document.querySelectorAll('.bird');
@@ -60,7 +76,7 @@ function birdsCreate(numberBirds) {
                 
                 item.style.top = birdReturnCoordinateY + '%';
                 item.style.left = birdReturnCoordinateX + '%';
-            }
+            };
     
             setTimeout(birdReturn, 1000);
         
@@ -68,4 +84,4 @@ function birdsCreate(numberBirds) {
     });
 };
 
-birdsCreate(3);
+getBirds(3);
